54e5c6aec0ff717f463bf45b07f2b2f668e1f5e6
TMerco <Tmercolino@sea.yearup.org>
2022-07-19 10:43:31 -0700

    First Function

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,8 +1,13 @@
 //alert("It's working!");
-
+/*
 let actors = document.querySelectorAll(".hamlet");
 
 console.log(actors);
 
 document.querySelectorAll(".hamlet").style.backgroundColor = "yellow";
- 
+ */
+
+
+function highlight(el){
+    el.style.backgroundColor='blue';
+}
