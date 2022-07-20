1c72014e7c957f28e6ffbdc6d3dda05de832abff
TMerco <Tmercolino@sea.yearup.org>
2022-07-20 11:06:07 -0700

    adds annonymous function

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,4 +1,5 @@
-/*alert("It's working!");
+/*
+alert("It's working!");
 let actors = document.querySelectorAll(".hamlet");
 
 console.log(actors);
@@ -13,11 +14,17 @@ let spans = document.querySelectorAll("#play span");
 
 console.log(spans);
 
+/*Annonymous function allows ANY code to be placed inside the curly brackets {} */
 for(const mySpan of spans){
-    mySpan.addEventListener("click",myAlert);
+    mySpan.addEventListener("click",function(ev){
+        highlightActor(mySpan.dataset.actor);
+    });
+   // alert(mySpan.dataset.actor);
 }
 
-
+function highlightActor(actor){
+    alert("The current actor is: " + actor);
+}
 
 
 
